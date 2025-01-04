1972d493f8571d15504cff8e5179ae243c48b09a
Ankit Shukla <134011141+Ankitshukla6121@users.noreply.github.com>
2025-01-04 22:31:22 +0530

    Update confing.js

--- backend/confing.js
+++ backend/confing.js
@@ -1,2 +1,2 @@
-export const PORT=5555; 
-export const mongoDBURL= 'mongodb+srv://root:root@book-store.cv8c8.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store';
+export const PORT = process.env.PORT || 5555;
+export const mongoDBURL = process.env.MONGO_DB_URL;
