59d7ec577d6285c61298a6a86b383d784583b53c
Ankit Shukla <134011141+Ankitshukla6121@users.noreply.github.com>
2025-01-04 16:23:53 +0530

    Update and rename config.js to .env

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT=5555; 
-
-// export const mongoDBURL= 'mongodb+srv://root:root@book-store.cv8c8.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store';
-export const mongoDBURL= 'mongodb+srv://root:root@book-store.cv8c8.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store';
