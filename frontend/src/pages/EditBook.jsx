2e182da25bb22df9b6f8f59bb55ed42e81eb5cf0
Ankit Shukla <134011141+Ankitshukla6121@users.noreply.github.com>
2025-01-06 22:14:40 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -16,7 +16,7 @@ const EditBook = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`https://book-net-backend.vercel.app/${id}`)
+    axios.get(`https://book-net-pz2e.vercel.app/books/${id}`)
     .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear)
